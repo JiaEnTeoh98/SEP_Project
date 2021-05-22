238575d70cec10ebba24f0e8104dd17ecc22de5f
JiaEnTeoh98 <83648535+JiaEnTeoh98@users.noreply.github.com>
2021-05-22 23:55:09 +0800

    new branch, create new views
    create rider pickup list, rider delivery list, rider pickup and delivery php

---
+++ DERCS/Project/resources/views/RiderPickupList.blade.php
@@ -0,0 +1,64 @@
+<html>
+    <head>
+        <title>Rider Pickup List</title>
+        
+    </head>
+    <style>
+        
+        th, td {
+        padding: 5px;
+        vertical-align: top;
+        text-align: left;
+        border:1px solid black;    
+        }
+
+        
+
+        .button{
+            background-color:BCBDFD ;
+            color:black;
+            border-radius:4px;
+            border-width:1px;
+            height:25px;
+            width:100px;
+            font-family:Times New Roman;
+        }
+    </style>
+    
+   
+    
+    <body>
+    <br>
+    <h1>Rider Pickup List</h2>
+        <br>
+        
+        <!--To show customer list waiting for pickup-->
+        <form action="" method="POST">
+        <table style="border: 1px solid black;
+            border-collapse: collapse;padding: 7px;
+        vertical-align: top;text-align: left;">
+        <thead>
+        <th>No</th>
+        <th>Quotation ID</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Action</th>
+        <tr>
+        <td>1</td>
+        <td>0001</td>
+        <td>Teoh Jia En</td>
+        <td>12, Lorong Sentosa</td>
+        <td><button type="submit" name="Access" value="Access" class="button">Access</td>
+        
+        </tr>
+        </table>
+        
+        
+        <br><br>
+        
+       </form>
+       
+    
+    </body>
+    
+</html>
